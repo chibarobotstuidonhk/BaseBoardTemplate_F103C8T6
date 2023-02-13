14198368103748445377654fe3ab756c7278a76d
Yamada Yuhei <tamanisikaminai@gmail.com>
2023-02-14 00:14:14 +0900

    first commit

---
+++ Core/Src/wapper.cpp
@@ -0,0 +1,20 @@
+/*
+ * wapper.cpp
+ *
+ *  Created on: Feb 14, 2023
+ *      Author: YMD
+ */
+#include "main.h"
+
+
+void can_rx_callback(CAN_HandleTypeDef *hcan){
+
+}
+
+void setup(){
+
+}
+
+void loop(){
+
+}
